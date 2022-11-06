cae548b5e4f3d9f571d77e66df62a63ac8b23a76
P@ul <MyEmail@Zoho.com>
2022-11-06 22:08:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://firecrud-486cd-default-rtdb.firebaseio.com/movies.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      let loadedMovies = []
+
+      for (const key in data) {
+        console.log(data[key]);
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+          ,
+        })
+
+      }
+
+      /*  const transformedMovies = data.results.map((movieData) => {
+         return {
+           id: movieData.episode_id,
+           title: movieData.title,
+           openingText: movieData.opening_crawl,
+           releaseDate: movieData.release_date,
+         };
+       }); */
+
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://firecrud-486cd-default-rtdb.firebaseio.com/movies.json', {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': "application/json"
+      }
+    })
+
+    toast("Wow so easy !");
+    const data = await response.json()
+
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+        <ToastContainer />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
